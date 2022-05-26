4b03352dc4042ac8ccea95cf1246531b1a5951f3
Justinas <justasrimavicius@gmail.com>
2022-05-26 22:01:22 +0300

    Deleted uneccessary code lines. Fixed the calculation logic. Left to do: find calculating bugs and fix them; Error messages in certain scenarios(when = is pressed without any number and operation inputs); Erase function.

--- script.js
+++ script.js
@@ -11,51 +11,36 @@ let x=0;
 // TODO: when number inputs go after an operation input, it overrides the whole input text and pushes out the the current.
 numberBoxes.forEach(element=>{
     element.addEventListener('click',function(){
+        if(inputField.innerText.slice(-1)=='-'||inputField.innerText.slice(-1)=='+'||inputField.innerText.slice(-1)=='/'||inputField.innerText.slice(-1)=='*'){//Check if previous input was an operator. If it was, that means the next number inputed should be stored in the next array index.
+            x++;
+        }
         if(inputArray[x]==undefined){
             inputArray[x]=element.innerText;
-
         }else{
             inputArray[x]+=element.innerText;
-
         };
-        console.log(x);
-
-        // if(inputField.innerText.slice(-1)=='-'||inputField.innerText.slice(-1)=='+'||inputField.innerText.slice(-1)=='/'||inputField.innerText.slice(-1)=='*'){
-        //     inputField.innerText+=element.innerText;
-        // }else{
-        // inputNum+=element.innerText;
-        // inputField.innerText=inputNum;
-        // }
+        inputField.innerText+=element.innerText;
+        console.log(inputArray);
     });
 });
 
 const operationBoxes=document.querySelectorAll('.operation');
 
-let operation='';
 
 operationBoxes.forEach(element=>{
     element.addEventListener('click', function(){
-        if(element.innerText!='erase'){
-            if(inputArray[x]!=undefined)inputField.innerText+=inputArray[x];
-            if(inputField.innerText.slice(-1)=='-'||inputField.innerText.slice(-1)=='+'||inputField.innerText.slice(-1)=='/'||inputField.innerText.slice(-1)=='*'){
-                inputField.innerText=inputField.innerText.replace(inputField.innerText.slice(-1),element.innerText);
-                operation=element.innerText;
-                console.log(operation);
-                operatorArray[y]=operation;
-            }else{
-
-                operation=element.innerText;
-                inputField.innerText+=operation;
-                console.log(inputArray);
-                operatorArray[y]=operation;
+            if(inputField.innerText.slice(-1)=='-'||inputField.innerText.slice(-1)=='+'||inputField.innerText.slice(-1)=='/'||inputField.innerText.slice(-1)=='*'){ //Checks if the users last input was a number or an operation
+                inputField.innerText=inputField.innerText.replace(inputField.innerText.slice(-1),element.innerText); //If it was an operation, override the previous one with the new one
+                operatorArray[y-1]=element.innerText;
+                console.log(operatorArray[y-1]);
+            }else{ //If it wasn't, just make a new one
+                inputField.innerText+=element.innerText;
+                operatorArray[y]=element.innerText;
                 y++;
-
             }
-
             console.log(operatorArray);
-            x++;
 
-        }
+
     });
 });
 
@@ -67,30 +52,38 @@ function PushToInput(inputNum,operation){
 
 
 equal.addEventListener('click',function(){
-    let operate={
-        '+':function(a,b){return a+b},
-        '-':function(a,b){return a-b},
-        '*':function(a,b){return a*b},
-        '/':function(a,b){return a/b},
-    };
+function operate(a,b,operatorKey){
+    if(operatorKey=='+')return parseFloat(a)+parseFloat(b);
+    if(operatorKey=='-')return parseFloat(a)-parseFloat(b);
+    if(operatorKey=='*')return parseFloat(a)*parseFloat(b);
+    if(operatorKey=='/')return parseFloat(a)/parseFloat(b);
+}
     let index;
     let answer=0;
     let temp=0;
-    inputArray.forEach(element => {
-        if((!operatorArray.includes('*'))&&(!operatorArray.includes('/'))){
-            answer=operate[operatorArray](answer,element);
+    let z=0;
+console.log('');
+console.log('');
 
-        }
-
-
-
-
-    });
-
+inputArray.forEach(element => {
     console.log(answer);
+    if(element==inputArray[0]){
+        temp=element;
+    }
+    else{
+        answer=operate(temp,element,operatorArray[z]);
+        z++;
+        temp=answer;
+    }
+    if(inputArray[(inputArray.length)-1]==element){
+        console.log(answer);
+        document.querySelector('.answer').innerText=answer;
+    }
+    console.log(temp);
+    console.log('');
+});
 
 
 
-
-})
+});
 
